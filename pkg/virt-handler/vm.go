4cab598c1c7890a6270b83736014dd9f77a9c10d
Enrique Llorente <ellorent@redhat.com>
2023-02-14 12:37:57 +0100

    dnm: Disable live migration network check
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- pkg/virt-handler/vm.go
+++ pkg/virt-handler/vm.go
@@ -2289,9 +2289,11 @@ func (d *VirtualMachineController) checkNetworkInterfacesForMigration(vmi *v1.Vi
 		return nil
 	}
 
-	if !netvmispec.IsPodNetworkWithMasqueradeBindingInterface(vmi.Spec.Networks, ifaces) {
-		return fmt.Errorf("cannot migrate VMI which does not use masquerade to connect to the pod network")
-	}
+	/*
+		if !netvmispec.IsPodNetworkWithMasqueradeBindingInterface(vmi.Spec.Networks, ifaces) {
+			return fmt.Errorf("cannot migrate VMI which does not use masquerade to connect to the pod network")
+		}
+	*/
 
 	return nil
 }
